09f9f093a501bd98a362c63f198b167d9e09b63e
Aimad <aimadyahyaoui@icloud.com>
2026-07-24 10:18:03 +0200

    Add POST tasks endpoint

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,10 @@
 import express, {type Express, type Request, type Response} from "express";
 import { tasksMock } from "./data/tasks.js";
+import type { Task, TaskCreate } from "./types/tasks.js";
 
 const app: Express = express();
+
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
     res.json({message : "API is running"});
@@ -9,6 +12,22 @@ app.get("/", (req: Request, res: Response) => {
 
 app.get("/tasks", (req: Request, res: Response) => {
     res.json(tasksMock)
+})
+
+app.post("/tasks", (req: Request, res: Response) => {
+    console.log(req.body);
+    const newTask: TaskCreate = {
+        title: req.body.title.toString(),
+        description: req.body.description.toString(),
+        assignedId: "",
+        dueDate: req.body.dueDate,
+        priority: "low",
+        status: "todo"
+    }
+    const task: Task = {id: crypto.randomUUID(),...newTask}
+    tasksMock.push(task)
+    // res.send(task)
+    res.status(200).json(task)
 })
 
 export default app;
